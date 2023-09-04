23fbdf22a5ad17b722ab9d6d5bccfa8901f1bc84
Sergey Zaytsev <s.zaytsev@adspend.ru>
2023-09-04 10:13:35 +0300

    Update agent main

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,3 +1,85 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"io"
+	"net"
+	"net/http"
+	"net/url"
+	"os"
+	"path"
+	"time"
+
+	"github.com/sergeizaitcev/metrics/internal/metrics"
+)
+
+const (
+	host = "localhost"
+	port = "8080"
+)
+
+const (
+	pollInterval  = 2 * time.Second
+	reportInteval = 10 * time.Second
+)
+
+func main() {
+	if err := run(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+func run() error {
+	pollTicker := time.NewTicker(pollInterval)
+	defer pollTicker.Stop()
+
+	reportTicker := time.NewTicker(reportInteval)
+	defer reportTicker.Stop()
+
+	for {
+		var report bool
+
+		select {
+		case <-pollTicker.C:
+		case <-reportTicker.C:
+			report = true
+		}
+
+		snapshot := metrics.Snapshot()
+		if !report {
+			continue
+		}
+
+		for _, metric := range snapshot {
+			if err := reportMetric(metric); err != nil {
+				fmt.Fprintln(os.Stderr, err)
+				break
+			}
+		}
+	}
+}
+
+func reportMetric(m metrics.Metric) error {
+	u := &url.URL{
+		Scheme: "http",
+		Host:   net.JoinHostPort(host, port),
+	}
+
+	switch m.Kind() {
+	case metrics.KindCounter:
+		u.Path = path.Join("update", "counter", m.Name(), m.String())
+	case metrics.KindGauge:
+		u.Path = path.Join("update", "gauge", m.Name(), m.String())
+	default:
+		return fmt.Errorf("unknown metric kind: %s", m.Kind())
+	}
+
+	res, err := http.Post(u.String(), "text/plain; charset=utf-8", nil)
+	if err != nil {
+		return err
+	}
+
+	io.Copy(io.Discard, res.Body)
+	return res.Body.Close()
+}
