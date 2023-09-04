273cf27e43dbf3cb3f129b81bd1d165076c052a8
Sergey Zaytsev <s.zaytsev@adspend.ru>
2023-09-04 10:06:40 +0300

    Update main

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,30 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"net"
+	"net/http"
+	"os"
+
+	"github.com/sergeizaitcev/metrics/internal/handlers"
+	"github.com/sergeizaitcev/metrics/internal/storage/local"
+)
+
+const (
+	host = "localhost"
+	port = "8080"
+)
+
+func main() {
+	if err := run(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+func run() error {
+	storage := local.NewStorage()
+	handler := handlers.NewMetrics(storage)
+	addr := net.JoinHostPort(host, port)
+	return http.ListenAndServe(addr, handler)
+}
