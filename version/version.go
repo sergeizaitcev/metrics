afe9bfdf02513e30f9b477b1e900697f7bff9c52
Sergei Zaitcev <zzzaitcevsergei@yandex.ru>
2024-01-11 10:16:29 +0300

    Review fix 1

--- version/version.go
+++ version/version.go
@@ -2,8 +2,6 @@ package version
 
 import (
 	"fmt"
-	"io"
-	"os"
 )
 
 var (
@@ -14,10 +12,5 @@ var (
 
 // Print печатает в стандартный вывод информацию о сборке.
 func Print() {
-	PrintTo(os.Stdout)
-}
-
-// PrintTo печатает в w справочную информацию о сборке.
-func PrintTo(w io.Writer) {
-	fmt.Fprintf(w, "Build version: %s\nBuild date: %s\nBuild commit: %s\n", Build, Date, Commit)
+	fmt.Printf("Build version: %s\nBuild date: %s\nBuild commit: %s\n", Build, Date, Commit)
 }
