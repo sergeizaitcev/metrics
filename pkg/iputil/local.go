6b804f24181a33602ef5e52274e56f04804d206b
Sergei Zaitcev <zzzaitcevsergei@yandex.ru>
2024-01-27 17:06:36 +0300

    Add iputil package

---
+++ pkg/iputil/local.go
@@ -0,0 +1,16 @@
+package iputil
+
+import "net"
+
+// Local возвращает локальный IP-адрес.
+func Local() net.IP {
+	addrs, _ := net.InterfaceAddrs()
+	for _, addr := range addrs {
+		if ipnet, ok := addr.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				return ipnet.IP
+			}
+		}
+	}
+	return nil
+}
